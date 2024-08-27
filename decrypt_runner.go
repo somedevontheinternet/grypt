cbe0b126902ad9de4ac05b3ad57044d226cd1bf6
somedevontheinternet <somedevontheinternet@gmail.com>
2024-08-27 11:54:34 -0400

    v1.1 parallel decryption

--- decrypt_runner.go
+++ decrypt_runner.go
@@ -4,40 +4,46 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
+	"sync"
+	"time"
 )
 
 type DecryptRunner struct {
 	Key          string
 	DecryptedDir string
 	EncryptDir   string
-	Meta         Meta
 }
 
-func NewDecryptRunner(key, encryptDir, decryptedDir string, meta Meta) *DecryptRunner {
-	return &DecryptRunner{
-		Key:          key,
-		DecryptedDir: decryptedDir,
-		EncryptDir:   encryptDir,
-		Meta:         meta,
-	}
-}
-
-func (r *DecryptRunner) Run(meta Meta) {
-	prepareDir(*OutRoot)
-	for _, file := range meta {
-		fmt.Printf("DECRYPTING: %s\n", file.DecryptedName)
-		inPath := filepath.Join(r.EncryptDir, file.EncryptedName)
-		outPath := filepath.Join(r.DecryptedDir, file.DecryptedName)
+func NewDecryptRunner(key, encryptDir, decryptedDir string, jobs chan FileMeta, wg *sync.WaitGroup) {
+	for job := range jobs {
+		fmt.Printf("DECRYPTING: %s\n", job.DecryptedName)
+		inPath := filepath.Join(encryptDir, job.EncryptedName)
+		outPath := filepath.Join(decryptedDir, job.DecryptedName)
 		dir := filepath.Dir(outPath)
 		os.MkdirAll(dir, 0755)
-		Decrypt(r.Key, inPath, outPath)
-		os.Chtimes(outPath, file.ModTime, file.ModTime)
+		Decrypt(key, inPath, outPath)
+		os.Chtimes(outPath, job.ModTime, job.ModTime)
+		wg.Done()
 	}
 }
 
 func PerformDecryption() {
 	key := GetEncryptionKey(false)
 	meta := GetMeta(key)
-	r := NewDecryptRunner(key, *BlobsRoot, *OutRoot, meta)
-	r.Run(meta)
+	prepareDir(*OutRoot)
+	runners := runtime.NumCPU()
+	var wg sync.WaitGroup
+	jobs := make(chan FileMeta)
+	for i := 0; i < runners; i++ {
+		go NewDecryptRunner(key, *BlobsRoot, *OutRoot, jobs, &wg)
+	}
+	before := time.Now()
+	for _, file := range meta {
+		wg.Add(1)
+		jobs <- file
+	}
+	wg.Wait()
+	after := time.Now()
+	fmt.Printf("Decryption completed in %s\n", (after.Sub(before)))
 }
